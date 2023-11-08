b9bbc5ce5ec6694415254dbfc3c9f5f9abb43e02
decimechochi <rocio.sgo@gmail.com>
2023-11-07 21:14:52 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express ();
+const path = require ("path");
+
+app.use(express.static(__dirname + "/public"));
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, ()=>{
+    console.log(
+        `Servidor corriendo en el puerto ${port}`);
+});
+
+app.listen(3008, () => {
+    console.log("servidor corriendo en puerto 3008");
+});
+
+app.get("/", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/register", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+
