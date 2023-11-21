3d51aec24f7b65c29b3163a76745ec9ba7108f7f
Rocio Diaz <rocio.sgo@gmail.com>
2023-11-21 20:18:06 -0300

    despues lo sigo viendo. agregue ejs

--- Controller/productosController.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const productosController = {
-    detalle: (req,res) => {
-        res.sendFile(path.resolve(__dirname, "../views/detale.html"))
-    },
-}
-
-module.export = productosController;
